615985e309e87f1d08bf38f82c6af7927e5137c7
sergey <rifstop1975@gmail.com>
2025-10-23 20:13:18 +0600

    создал модель Статей

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# описание модели базы данных
+
+class Article(models.Model):
+    title = models.CharField(max_length=50,
+                             null=False, blank=False,
+                             verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Контент')
+    author = models.CharField(max_length=50,default='Anonymous',verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name='дата создания')
+    update_at = models.DateTimeField(auto_now_add=True, verbose_name='дата обновления')
+
+    def __str__(self):
+        return  f'{self.id} - {self.title}'
