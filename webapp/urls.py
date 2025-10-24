4006e66abd8f0510950157fc3d010f402e50aaa8
sergey <rifstop1975@gmail.com>
2025-10-24 17:20:35 +0600

    обновлен views

---
+++ webapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from webapp.views import article_list_view, article_detail_view, article_create_view
+
+urlpatterns =[
+    path('', article_list_view),
+    path('article/', article_detail_view),
+    path('article/create/', article_create_view),
+]
