daf02572e6e3631946d4554813e8ec8f935f132d
sergey <rifstop1975@gmail.com>
2025-11-11 11:41:50 +0600

    создали модели Tag и Comment через классы

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import widgets
 
+from  webapp.models import  Tag
+
 #from webapp.models import status_choices
 
 class ArticleForm(forms.Form):
@@ -8,4 +10,5 @@ class ArticleForm(forms.Form):
     author = forms.CharField(max_length=50, required=True, label='Автор', widget=widgets.Input(attrs={'class': 'form-control'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}), required=True, label='Контент')
 
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(),required=False, label='Теги')
     # status = forms.ChoiceField(choices=status_choices, label='Статус', widget=widgets.RadioSelect)
--- webapp/views.py
+++ webapp/views.py
@@ -1,33 +1,39 @@
-from django.http import HttpResponseRedirect, HttpResponseNotFound, Http404
+
 from django.shortcuts import render, get_object_or_404, reverse, redirect
-from django.template.defaultfilters import title
+from django.views.generic import View, TemplateView, CreateView
 
 from webapp.models import Article
-
 from webapp.forms import ArticleForm
 
-from webapp.validation import validate
 
 
 # Create your views here.
 
-def article_list_view(request):
-    articles = Article.objects.all()
-    context = {
-        'articles': articles,
-    }
-    return render(request,'article_list.html', context)
 
-def article_detail_view(request,*args, pk, **kwargs):
-    article = get_object_or_404(Article, pk=pk)
-    return render(request, 'article_detail.html', {'article': article})
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()
+        context = {
+            'articles': articles,
+        }
+        return render(request, 'article_list.html', context)
 
-def article_create_view(request):
-    if request.method == 'GET':
+
+
+class ArticleDetailView(TemplateView):
+    template_name = 'article_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['article'] = get_object_or_404(Article, pk=self.kwargs.get('pk'))
+        return context
+
+
+class ArticleCreateView(View):
+    def get(self, request, *args, **kwargs):
         form = ArticleForm()
-        return  render(request, 'article_create.html' , {'form': form})
-    elif request.method == 'POST':
-#        errors = validate(request.POST)
+        return render(request, 'article_create.html', {'form': form})
+    def post(self, request, *args, **kwargs):
         form = ArticleForm(data=request.POST)
         if form.is_valid():
             article = Article()
@@ -35,22 +41,17 @@ def article_create_view(request):
             article.content=form.cleaned_data.get('content')
             article.author=form.cleaned_data.get('author')
             article.save()
+            article.tags.set(form.cleaned_data.get('tags'))
             return redirect('article_detail', pk=article.id)
-#        if errors:
         return render(request, 'article_create.html', {'form': form})
-#        title=request.POST.get('title')
-#        content=request.POST.get('content')
-#        author=request.POST.get('author')
-#        article=Article.objects.create(title=title, content=content, author=author)
-#        article.save()
-#        return HttpResponseRedirect(reverse('article_list'))
-#        return redirect('article_list')
-#        return redirect('article_detail', pk=article.id)
+
+
+
 
 def article_update_view(request, *args, pk, **kwargs):
     article = get_object_or_404(Article, pk=pk)
     if request.method == "GET":
-        form = ArticleForm(initial={'title': article.title, 'content': article.content, 'author': article.author})
+        form = ArticleForm(initial={'title': article.title, 'content': article.content, 'author': article.author, 'tags': article.tags.all()})
         return render(request, 'article_update.html', {'form': form})
     elif request.method == "POST":
         form = ArticleForm(data=request.POST)
@@ -59,6 +60,7 @@ def article_update_view(request, *args, pk, **kwargs):
             article.content = form.cleaned_data.get('content')
             article.author = form.cleaned_data.get('author')
             article.save()
+            article.tags.set(form.cleaned_data.get('tags'))
             return redirect('article_detail', pk=article.id)
         return render(request, 'article_update.html', {'form': form})
 
